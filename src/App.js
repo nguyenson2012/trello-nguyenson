f718d109ed6db7a971e8f231fa983c32a4d83821
nguyenson2012 <traibachkhoa2012@gmail.com>
2022-08-01 22:57:22 +0700

    Add design trello

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
+    <div className="trello-nguyenson-master">
+      <nav className="navbar app">App Bar</nav>
+      <nav className="navbar board">Board</nav>
     </div>
   );
 }
