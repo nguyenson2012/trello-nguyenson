a34ededef45507dbac3c7dc4333125e35b97c843
nguyenson2012 <traibachkhoa2012@gmail.com>
2022-08-02 08:24:01 +0700

    update design trello

--- src/App.js
+++ src/App.js
@@ -5,6 +5,36 @@ function App() {
     <div className="trello-nguyenson-master">
       <nav className="navbar app">App Bar</nav>
       <nav className="navbar board">Board</nav>
+      <div className="board-columns">
+        <div className="column">
+          <headers>Brainstorm</headers>
+          <ul>
+            <li>
+            <img src="https://i0.wp.com/duongnhuquynh.com/wp-content/uploads/2022/07/pexels-photo-5778899.jpeg" alt="anh-minh-hoa"></img>
+            </li>
+            <li>Add what you want below</li>
+            <li>Add what you want below</li>
+            <li>Add what you want below</li>
+            <li>Add what you want below</li>
+            <li>Add what you want below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <headers>Brainstorm</headers>
+          <ul>
+            <li>
+            <img src="https://i0.wp.com/duongnhuquynh.com/wp-content/uploads/2022/07/pexels-photo-5778899.jpeg" alt="anh-minh-hoa"></img>
+            </li>
+            <li>Add what you want below</li>
+            <li>Add what you want below</li>
+            <li>Add what you want below</li>
+            <li>Add what you want below</li>
+            <li>Add what you want below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
     </div>
   );
 }
